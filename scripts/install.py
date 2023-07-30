6456ba38125620c500fbfbe5d8f32111eda22f07
Oleg <razorback456@gmail.com>
2023-07-30 23:31:31 +0300

    Update install.py

--- scripts/install.py
+++ scripts/install.py
@@ -1,4 +1,4 @@
 import launch
 
 if not launch.is_installed("pyAesCrypt"):
-    launch.run_pip("install pyAesCrypt", "pyAesCrypt for Styles Editor")
+    launch.run_pip("install pyAesCrypt", "pyAesCrypt for UAStyle")
